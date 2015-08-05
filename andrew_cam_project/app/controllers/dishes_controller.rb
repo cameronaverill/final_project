00c65ba5b219a793b67302b694e1150e596212db
cameronaverill <cameronaverill2018@u.northwestern.edu>
2015-08-05 14:16:33 +0200

    heroku push

--- andrew_cam_project/app/controllers/dishes_controller.rb
+++ andrew_cam_project/app/controllers/dishes_controller.rb
@@ -15,14 +15,17 @@ class DishesController < ApplicationController
 
 	def add_dish
 		@dish = Dish.find(params[:id])
-	    current_user.dishes << @dish
+		unless current_user.dishes.find(@dish.id)
+	    	current_user.dishes << @dish
+	    end
+	    flash[:notice] = "Snack successfully saved"
 	    redirect_to dish_path(@dish)
 	end
 
 
 	private
 		def dish_params
-			params.require(:dish).permit(:name, :url, :query_id, :vegan, :servings, :vegetarian, :cheap, :gluten_free, :user_ids => [])
+			params.require(:dish).permit(:name, :url, :query_id, :vegan, :servings, :vegetarian, :cheap, :gluten_free, :image, :user_ids => [])
 		end
 
 end
